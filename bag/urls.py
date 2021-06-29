4ce06b8f3373492371035fc8d4b87bc37cf4d165
ErnestaMajute <ernesta.majute@gmail.com>
2021-06-29 14:41:25 +0000

    Added urls for bag modification and to delete product

--- bag/urls.py
+++ bag/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
     path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
+    path('modify/<item_id>/', views.modify_bag, name='modify_bag'),
+    path('delete/<item_id>/', views.delete_from_bag, name='delete_from_bag'),
 ]
