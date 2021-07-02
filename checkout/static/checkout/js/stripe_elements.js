a38452a2a45b4bb861c2f1c27e1c89c9b5f91ced
ErnestaMajute <ernesta.majute@gmail.com>
2021-07-02 19:01:51 +0000

    Completed webhook handler

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -104,7 +104,7 @@ form.addEventListener('submit', function(ev) {
                     </span>
                     <span>${result.error.message}</span>`;
                 $(errorDiv).html(html);
-                $('#payment-form').fadeToggle(100);
+                $('#payment-form').fadeToggle(9000);
                 card.update({ 'disabled': false});
                 $('#submit-button').attr('disabled', false);
             } else {
