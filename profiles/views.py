127705fa921a59db5876fdc2ba8d323fb047740c
ErnestaMajute <ernesta.majute@gmail.com>
2021-07-03 18:52:37 +0000

    Added add product functionality

--- profiles/views.py
+++ profiles/views.py
@@ -4,6 +4,9 @@ from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
 
+from .models import UserProfile
+from .forms import UserProfileForm
+
 from checkout.models import Order
 
 
@@ -18,8 +21,10 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
-
-    form = UserProfileForm(instance=profile)
+        else:
+            messages.error(request, 'Update failed. Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
