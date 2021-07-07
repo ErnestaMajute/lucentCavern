c3a801cef66694e66914da462e11e334a0fbbf26
ErnestaMajute <ernesta.majute@gmail.com>
2021-07-07 12:36:15 +0000

    Created Subscriptions app, added urls, added model, views

--- lucent_cavern/settings.py
+++ lucent_cavern/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'checkout',
     'profiles',
     'favorites',
+    'subscriptions',
 
     # Other
     'crispy_forms',
---
+++ subscriptions/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import redirect, get_object_or_404
+from django.contrib import messages
+
+from .models import SubscriptionUser
+
+
+def subscriptions_join(request):
+    redirect_url = request.POST.get('redirect_url')
+
+    if request.method == "POST":
+        email = request.POST.get('subscription_email')
+        if email:
+            try:
+                if_in_subscriptions_list = get_object_or_404(
+                    SubscriptionUser, email=email)
+                messages.error(
+                    request, f'{email} already subscribing our newsletter.')
+            except:
+                new_subscription = SubscriptionUser(email=email)
+                new_subscription.save()
+                messages.success(
+                    request, f'Thanks! {email} added to our subscription list!')
+        else:
+            messages.error(request, 'Please insert a valid email address.')
+
+    return redirect(redirect_url)
