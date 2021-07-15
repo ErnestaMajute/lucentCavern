a48888b82e65dd3ff123e707f115e9d9c90c6101
ErnestaMajute <ernesta.majute@gmail.com>
2021-07-15 06:34:41 +0000

    Context moved to return

--- profiles/views.py
+++ profiles/views.py
@@ -43,10 +43,6 @@ def order_history(request, order_number):
         'A confirmation email was sent on the order date.'
     ))
 
-    template = 'checkout/checkout_success.html'
-    context = {
-        'order': order,
-        'from_profile': True,
-    }
-
-    return render(request, template, context)
+    return render(request,
+                  'checkout/checkout_success.html',
+                  {'order': order, 'from_profile': True, })
